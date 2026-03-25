80d98254b522858311d8f83fdbc702f674e6c254
ElielRegiani <elielregiani@gmail.com>
2026-03-24 21:24:14 -0300

    first commit

---
+++ template-service/src/test/kotlin/whatsapp_platform/template_service/TemplateServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package whatsapp_platform.template_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TemplateServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
